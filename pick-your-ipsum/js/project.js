2f36f3f9d4cddc3f34cd01a8ec624f91c53f33a2
jaylupus <jonahwolf@jonah-wolfs-macbook-pro.local>
2015-04-14 16:37:49 -0400

    Fixed for repeat use

--- pick-your-ipsum/js/project.js
+++ pick-your-ipsum/js/project.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 	$('#paragraphs').click(function(){
 		$(this).val('');
 		$('.ipsum').slideUp();
+		$('p').hide();
 	});
 	$('#submit').click(function(){
 		for (i = 0; i < $('#paragraphs').val(); i++){
